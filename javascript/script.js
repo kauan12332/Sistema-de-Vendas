0f0298f802951876eee90b694138dbc5ecede7c9
KAUA ANDRADE <kauanandrademk@gmail.com>
2025-05-21 21:43:16 -0300

    upadate + pwa inserida

--- javascript/script.js
+++ javascript/script.js
@@ -234,3 +234,8 @@ function atualizarGrafico() {
     }
   });
 }
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('sw.js')
+    .then(() => console.log('✅ Service Worker registrado com sucesso!'))
+    .catch(err => console.error('❌ Erro ao registrar o Service Worker:', err));
+}
